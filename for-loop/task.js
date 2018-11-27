60425928c9b049216214bda0b40a397a7a4d811a
Stanis Amnell <StanislavTopchyi@gmail.com>
2018-11-27 13:17:33 +0200

    Final commit

--- for-loop/task.js
+++ for-loop/task.js
@@ -1,9 +1,21 @@
 function listAll(min, max) {
-  var result = '1 2 3 ';
+  var result = "";
+  for (var i=min; min<=max; min++){
+    result = result + min + " ";
+  }
   return result.trim();
 }
 
+/////// console.log(listAll(-2, 6));
+
 function listBetweenDesc(min, max) {
-  var result = '3 2';
+  // var result = '3 2';
+  var result = "";
+  max = max - 1;
+  for (i=max; max>min; max--) {
+    result = result + max + " ";
+  }
   return result.trim();
 }
+
+///// console.log(listBetweenDesc(1, 10));
